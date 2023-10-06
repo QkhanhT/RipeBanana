94e145835996f808499aed8568c7e1c9b19d0156
Tim Chen <timchenut567@utexas.org>
2023-10-06 06:02:18 -0500

    Add hashing

--- Backend/structs.py
+++ Backend/structs.py
@@ -1,7 +1,10 @@
+import hashlib
+
 class userAccount():
     def __init__(self, n, p) -> None: #type annotation
         self.__name = n # __ makes it private
-        self.__password = p
+        hashPassword = hashlib.sha256(p.encode()).hexdigest() #strings must be encoded before hasing
+        self.__password = hashPassword
     
     def getName(self): 
         return self.__name
@@ -16,11 +19,13 @@ class accountList():
     def addAcc(self,acc): #self is a keyword, refers to the instance of the class
         self.__accounts.append(acc)
     
-    def signIn(self, account):
+    def signIn(self, username, password):
+        #gets hashed password from the password to match with available accounts
+        hashPassword = hashlib.sha256(password.encode()).hexdigest()
         for acc in self.__accounts:
-            if acc.getName == account.getName:
+            if acc.getName == username:
                 #successful login
-                if acc.getPassword == account.getPassword:
+                if acc.getPassword == hashPassword:
                     return acc
                 #wrong password
                 else:
@@ -28,6 +33,3 @@ class accountList():
         
         #account not found
         return None
-            
-                
-    
